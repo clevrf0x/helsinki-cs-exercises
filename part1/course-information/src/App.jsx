497d241eaab87417187086de7edbaaaca1401980
Favas M <favas@zennode.com>
2023-06-12 12:58:57 +0530

    Add: execises 1.5 course information step 5

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -1,38 +1,39 @@
 import PropTypes from 'prop-types';
 
 export default function App() {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
-  let total = parts.reduce((sum, part) => (sum += part.exercises), 0);
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={total} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
 
 // Components
-function Header({ course }) {
-  return <h1>{course}</h1>;
+function Header({ courseName }) {
+  return <h1>{courseName}</h1>;
 }
 Header.propTypes = {
-  course: PropTypes.string.isRequired,
+  courseName: PropTypes.string.isRequired,
 };
 
 function Content({ parts }) {
@@ -67,9 +68,15 @@ Part.propTypes = {
   }).isRequired,
 };
 
-function Total({ total }) {
+function Total({ parts }) {
+  let total = parts.reduce((sum, part) => (sum += part.exercises), 0);
   return <p>Number of exercises {total}</p>;
 }
 Total.propTypes = {
-  total: PropTypes.number.isRequired,
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired
+    })
+  ).isRequired,
 };
